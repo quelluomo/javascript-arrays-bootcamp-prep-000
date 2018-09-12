ae6ef6a50c3de732e3ad4dbf0e7694d75f831a92
Matteo Muscas <thatman.j@gmail.com>
2018-09-12 10:48:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(element, array) {
-  var newArray = [array, ...element];
+  var newArray = [...array, element];
   return newArray;
 }
 
