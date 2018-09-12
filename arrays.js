1bb815f6b1c30f04503a48c5260bd1270738f728
Matteo Muscas <thatman.j@gmail.com>
2018-09-12 07:05:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles",
+]
+var superheroines = ["catwoman", "she-hulk", "mystique"]
+superheroines.push("wonder woman")
+
+var cities = ["New York", "San Francisco"]
+cities.unshift("Philadelphia")
+
+
+
+function addElementToBeginningOfArray() {
+  const array = [1];
+  const newArray = ['foo'];
+}
+
+ 
