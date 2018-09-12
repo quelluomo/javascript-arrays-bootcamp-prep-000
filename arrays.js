ba2f9ddca13c08c7f3d6c763818797956bbbb6c0
Matteo Muscas <thatman.j@gmail.com>
2018-09-12 10:13:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,9 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 function removeElementFromBeginningOfArray(array){
   array.slice(1);
+  return array;
 }
 function destructivelyRemoveElementFromEndOfArray(array){
 array.pop();
+return array;
 }
